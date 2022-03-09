8c2c95e9e36eef70e0e202454e46aad23b726bc1
Millie Cheung <cheungmillie9@gmail.com>
2022-03-09 18:18:36 +0100

    removed unnecessary code

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,15 @@
-import { CSSProperties } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 
 function Layout() {
   return (
-    <div style={rootStyle}>
+    <div>
       <Header />
       <Outlet />
-      {/* <ProductCard /> */}
       <Footer />
     </div>
   );
 }
 
-const rootStyle: CSSProperties = {};
 export default Layout;
