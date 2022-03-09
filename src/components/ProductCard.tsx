4746429435510cc7b43556cd24de091de9013c54
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-03-09 11:08:03 +0100

    fixed the layout of the cards

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,169 +5,249 @@ import {
   CardActions,
   CardContent,
   CardMedia,
-  Container,
-  Grid,
   Typography,
 } from "@mui/material";
 import { CSSProperties } from "react";
 
 const rootStyle: CSSProperties = {
-  maxWidth: 345,
+  maxWidth: 330,
+  padding: "2rem",
 };
 const cardStyle: CSSProperties = {
   height: 250,
 };
+const divStyle: CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  margin: "2rem",
+  justifyContent: "space-evenly",
+};
 
 function ProductCard() {
   return (
-    <Container maxWidth="lg">
-      <Grid
-        container
-        direction="column"
-        justifyContent="space-evenly"
-        alignItems="center"
-      >
-        <Grid item xs={12} sm={4}>
-          <Card style={rootStyle}>
-            <CardActionArea>
-              <CardMedia
-                style={cardStyle}
-                image="../assets/products/soffbord.png"
-                title="table"
-              ></CardMedia>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  CATALINA sidobord 3-pack
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  3699:-
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Lägg till i varukorgen
-              </Button>
-            </CardActions>
+    <div style={divStyle}>
+      <Card style={rootStyle}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            style={cardStyle}
+            image=""
+            title=""
+          ></CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              NAME OF PRODUKT
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              PRICE HERE:-
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Lägg till i varukorgen
+          </Button>
+        </CardActions>
+      </Card>
 
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                style={cardStyle}
-                image="../assets/products/soffbord.png"
-                title="table"
-              ></CardMedia>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  CATALINA sidobord 3-pack
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  3699:-
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Lägg till i varukorgen
-              </Button>
-            </CardActions>
+      <Card style={rootStyle}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            style={cardStyle}
+            image="../assets/products/soffbord.png"
+            title="table"
+          ></CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              NAME OF PRODUKT
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              PRICE:-
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Lägg till i varukorgen
+          </Button>
+        </CardActions>
+      </Card>
 
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                style={cardStyle}
-                image="../assets/products/soffbord.png"
-                title="table"
-              ></CardMedia>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  CATALINA sidobord 3-pack
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  3699:-
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Lägg till i varukorgen
-              </Button>
-            </CardActions>
+      <Card style={rootStyle}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            style={cardStyle}
+            image="../assets/products/soffbord.png"
+            title="table"
+          ></CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              NAME OF PRODUKT
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              PRICE:-
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Lägg till i varukorgen
+          </Button>
+        </CardActions>
+      </Card>
 
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                style={cardStyle}
-                image="../assets/products/soffbord.png"
-                title="table"
-              ></CardMedia>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  CATALINA sidobord 3-pack
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  3699:-
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Lägg till i varukorgen
-              </Button>
-            </CardActions>
+      <Card style={rootStyle}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            style={cardStyle}
+            image="../assets/products/soffbord.png"
+            title="table"
+          ></CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              NAME OF PRODUKT
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              PRICE:-
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Lägg till i varukorgen
+          </Button>
+        </CardActions>
+      </Card>
 
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                style={cardStyle}
-                image="../assets/products/soffbord.png"
-                title="table"
-              ></CardMedia>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  CATALINA sidobord 3-pack
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  3699:-
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Lägg till i varukorgen
-              </Button>
-            </CardActions>
-
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                style={cardStyle}
-                image="../assets/products/soffbord.png"
-                title="table"
-              ></CardMedia>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  CATALINA sidobord 3-pack
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  3699:-
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Lägg till i varukorgen
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      </Grid>
-    </Container>
+      <Card style={rootStyle}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            style={cardStyle}
+            image="../assets/products/soffbord.png"
+            title="table"
+          ></CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              NAME OF PRODUKT
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              PRICE:-
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Lägg till i varukorgen
+          </Button>
+        </CardActions>
+      </Card>
+      <Card style={rootStyle}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            style={cardStyle}
+            image="../assets/products/soffbord.png"
+            title="table"
+          ></CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              NAME OF PRODUKT
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              PRICE:-
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Lägg till i varukorgen
+          </Button>
+        </CardActions>
+      </Card>
+      <Card style={rootStyle}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            style={cardStyle}
+            image="../assets/products/soffbord.png"
+            title="table"
+          ></CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              NAME OF PRODUKT
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              PRICE:-
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Lägg till i varukorgen
+          </Button>
+        </CardActions>
+      </Card>
+      <Card style={rootStyle}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            style={cardStyle}
+            image="../assets/products/soffbord.png"
+            title="table"
+          ></CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              NAME OF PRODUKT
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              PRICE:-
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Lägg till i varukorgen
+          </Button>
+        </CardActions>
+      </Card>
+      <Card style={rootStyle}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            style={cardStyle}
+            image="../assets/products/soffbord.png"
+            title="table"
+          ></CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              NAME OF PRODUKT
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              PRICE:-
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Lägg till i varukorgen
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
 
