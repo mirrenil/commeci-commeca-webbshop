b4e23162a2a7769267f356ae52dfa648a80d4392
Millie Cheung <cheungmillie9@gmail.com>
2022-03-09 14:53:28 +0100

    made header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,20 @@
-import { AppBar, Button, Toolbar } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { Box } from "@mui/system";
+import { MouseEvent, useState } from "react";
+import { Link } from "react-router-dom";
 import cartIcon from "../assets/icons/icon-shopping-cart.webp";
 import userIcon from "../assets/icons/icon-user.webp";
 import logo from "../assets/images/logo.svg";
-import { makeStyles } from "@mui/styles";
-import { Link } from "react-router-dom";
 
 interface Page {
   label: string;
@@ -11,12 +22,13 @@ interface Page {
 }
 
 function Header() {
-  const { header, navItems, logoStyle, icon, iconsContainer } = useStyles();
+  const [anchorMenu, setAnchorMenu] = useState(false);
+  const { logoStyle, icon, iconsContainer, menuItems } = useStyles();
 
   const menuLeft: Page[] = [
     {
       label: "Furniture",
-      href: "/furniture",
+      href: "/",
     },
     {
       label: "Textiles",
@@ -39,25 +51,23 @@ function Header() {
     },
   ];
 
-  const navbar = () => {
-    return (
-      <Toolbar>
-        {getMenuButtonsLeft()}
-        <img className={logoStyle} src={logo} alt="comme ci comme ca"></img>
-        {getMenuButtonsRight()}
-      </Toolbar>
-    );
+  const handleOpenMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorMenu(true);
+  };
+
+  const handleCloseMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorMenu(false);
   };
 
   const icons = () => {
     return (
       <div className={iconsContainer}>
-        {/* <Link to="/"> */}
-        <img className={icon} src={userIcon} alt="login/logout" />
-        {/* </Link>
-        <Link to="/"> */}
-        <img className={icon} src={cartIcon} alt="cart" />
-        {/* </Link> */}
+        <Link to="/account">
+          <img className={icon} src={userIcon} alt="login/logout" />
+        </Link>
+        <Link to="/checkoutpage">
+          <img className={icon} src={cartIcon} alt="cart" />
+        </Link>
       </div>
     );
   };
@@ -70,8 +80,12 @@ function Header() {
             key: label,
             color: "inherit",
             to: href,
-            // component: RouterLink,
-            className: navItems,
+          }}
+          sx={{
+            textTransform: "capitalize",
+            fontFamily: "Prata",
+            m: "2.5rem",
+            display: { xs: "none", sm: "none", md: "block", lg: "block" },
           }}
         >
           {label}
@@ -87,10 +101,15 @@ function Header() {
           {...{
             key: label,
             color: "inherit",
-            to: href,
-            // component: RouterLink,
-            className: navItems,
-          }}
+            component: <Link to={href}>,
+          }
+          sx={{
+            textTransform: "capitalize",
+            fontFamily: "Prata",
+            m: "2.5rem",
+            display: { xs: "none", sm: "none", md: "block", lg: "block" },
+          }}}
+          
         >
           {label}
         </Button>
@@ -99,27 +118,110 @@ function Header() {
   };
 
   return (
-    <AppBar className={header}>
-      {navbar()}
-      {icons()}
+    <AppBar
+      sx={{
+        backgroundColor: "#6C665F",
+        padding: "0.5rem 1rem",
+        placeContent: "center",
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <Toolbar
+        sx={{
+          width: "100%",
+          justifyContent: {
+            xs: "space-between",
+            sm: "space-between",
+            md: "space-around",
+            lg: "space-around",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: {
+              xs: "flex",
+              md: "none",
+            },
+          }}
+        >
+          <IconButton
+            size="large"
+            aria-label="menu"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenMenu}
+            sx={{
+              color: "white",
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorReference="anchorPosition"
+            anchorPosition={{ top: 75, left: 0 }}
+            keepMounted
+            open={Boolean(anchorMenu)}
+            onClose={handleCloseMenu}
+            sx={{
+              display: { xs: "block", md: "none" },
+            }}
+          >
+            {menuLeft.map((item) => (
+              <MenuItem
+                sx={{
+                  width: "200px",
+                }}
+                key={item.label}
+                onClick={handleCloseMenu}
+                component={Link}
+                to={item.href}
+              >
+                <Typography
+                  sx={{
+                    textTransform: "capitalize",
+                    fontFamily: "Prata",
+                    fontSize: ".9rem",
+                  }}
+                  textAlign="center"
+                >
+                  {item.label}
+                </Typography>
+              </MenuItem>
+            ))}
+            {menuRight.map((item) => (
+              <MenuItem
+                key={item.label}
+                onClick={handleCloseMenu}
+                component={Link}
+                to={item.href}
+              >
+                <Typography
+                  sx={{
+                    textTransform: "capitalize",
+                    fontFamily: "Prata",
+                    fontSize: ".9rem",
+                  }}
+                  textAlign="center"
+                >
+                  {item.label}
+                </Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+        {getMenuButtonsLeft()}
+        <img className={logoStyle} src={logo} alt="comme ci comme ca"></img>
+        {getMenuButtonsRight()}
+        {icons()}
+      </Toolbar>
     </AppBar>
   );
 }
 
 const useStyles = makeStyles(() => ({
-  header: {
-    background: "#6C665F",
-    padding: "0.5rem 1rem",
-    alignSelf: "center",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  navItems: {
-    textTransform: "capitalize",
-    fontFamily: "Prata",
-    margin: "2.5rem",
-  },
   logoStyle: {
     width: "80px",
     height: "80px",
@@ -133,6 +235,10 @@ const useStyles = makeStyles(() => ({
     width: "25px",
     height: "25px",
     margin: ".5rem",
+  },
+  menuItems: {
+    textTransform: "capitalize",
+    fontFamily: "Prata",
   },
 }));
 
