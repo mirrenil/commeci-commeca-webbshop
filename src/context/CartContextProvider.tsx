52f02a4a954624f6f410ec40bbed24a7fdfbccb6
Millie Cheung <cheungmillie9@gmail.com>
2022-03-10 11:45:07 +0100

    add product to cart array

--- src/context/CartContextProvider.tsx
+++ src/context/CartContextProvider.tsx
@@ -20,7 +20,21 @@ const CartProvider: FC = (props) => {
 
   const addToCart = async (product: ProductData) => {
     const cartItem: CartItemData = { ...product, quantity: 1 };
-    setCart([...cart, cartItem]);
+
+    // if the cartItem has the same ID as one of the objects in the cart array
+    if (cart.map((item) => item.id).includes(cartItem.id)) {
+      cart.map((item) => item.quantity++);
+      console.log("yes, ");
+      // console.log("cart: " + cart.map((item) => item.id));
+      // console.log("cartItem: " + cartItem.id);
+      console.log(cart);
+    } else {
+      console.log("no");
+      // console.log("cart: " + cart.map((item) => item.id));
+      // console.log("cartItem: " + cartItem.id);
+      setCart([...cart, cartItem]);
+      console.log(cart);
+    }
   };
 
   console.log(cart);
