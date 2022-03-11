0fd0d84888c9f405e1ec21a6d112e49fddd36c45
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-03-11 15:53:45 +0100

    made the data grid editable!
    Co-authored-by: Fia <fiababiakandersson@users.noreply.github.com>

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,38 +1,21 @@
-import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
 import { Button } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete"
+import DeleteIcon from "@mui/icons-material/Delete";
 
- function ProductList() {
-  
+function ProductList() {
   const columns = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "title", headerName: "Title", width: 130 },
-    { field: "price", headerName: "Price (SEK)", width: 130 },
-    {
-      field: "edit",
-      headerName: "Edit",
-      width: 130,
-      renderCell: () => (
-        <>
-          <input type="number" value="1" min="0" max="999"></input>
-          {/* <Button type="submit" size="small"
-          >
-              <EditIcon></EditIcon>
-          </Button> */}
-        </>
-      ),
-    },
+    { field: "id", headerName: "ID", width: 70, editable: true },
+    { field: "title", headerName: "Title", width: 130, editable: true },
+    { field: "price", headerName: "Price (SEK)", width: 130, editable: true },
+    { field: "quantity", headerName: "In stock", width: 130, editable: true },
     {
       field: "delete",
       headerName: "Delete",
       width: 130,
       renderCell: () => (
         <>
-          <Button
-            type="submit"
-            size="small"
-          >
+          <Button type="submit" size="small">
             <DeleteIcon></DeleteIcon>
           </Button>
         </>
@@ -40,60 +23,52 @@ import DeleteIcon from "@mui/icons-material/Delete"
     },
   ];
 
-
-  // const handleDelete = (clickedProduct) => {
-  //     setProduct(products.filter((product) => product.id !== clickedProduct.id));
-  //     console.log(clickedProduct.id);
-  // };
-
-
   const rows = [
-      { id: "1", title: 'Jolly', price: '1599'},
-      { id: "2", title: "Chardonne", price: "3919"},
-      { id: "3", title: 'Xander', price: '2299'},
-      { id: "4", title: 'Magnolia', price: '1299'},
-      { id: "5", title: "Bella", price: "999" },
-      { id: "6", title: "Essentials", price: "5499"},
-      { id: "7", title: "Dagny", price: "2999" },
-      { id: "8", title: "Daisy Bubble", price: "199" },
-      { id: "9", title: "Barrel", price: "1799 "},
-      { id: "10", title: "Alba", price: "3449 "},
-      { id: "11", title: "Vinvent", price: "5999 "},
-      { id: "12", title: "Charleston", price: "2899"},
-      { id: "13", title: "Betty", price: "2899 "},
-      { id: "14",  title: "Minna", price: "1199"},
-      { id: "15", title: "Pirri 2-pack", price: "2699"},
-      { id: "16", title: "Melisso", price: "4999"},
-      { id: "17",  title: "Ditti", price: "1999"},
-  ]
+    { id: "1", title: "Jolly", price: "1599", quantity: "2" },
+    { id: "2", title: "Chardonne", price: "3919", quantity: "20" },
+    { id: "3", title: "Xander", price: "2299", quantity: "30" },
+    { id: "4", title: "Magnolia", price: "1299", quantity: "55" },
+    { id: "5", title: "Bella", price: "999", quantity: "10" },
+    { id: "6", title: "Essentials", price: "5499", quantity: "33" },
+    { id: "7", title: "Dagny", price: "2999", quantity: "15" },
+    { id: "8", title: "Daisy Bubble", price: "199", quantity: "41" },
+    { id: "9", title: "Barrel", price: "1799 ", quantity: "37" },
+    { id: "10", title: "Alba", price: "3449", quantity: "55" },
+    { id: "11", title: "Vinvent", price: "5999", quantity: "3" },
+    { id: "12", title: "Charleston", price: "2899", quantity: "8" },
+    { id: "13", title: "Betty", price: "2899", quantity: "64" },
+    { id: "14", title: "Minna", price: "1199", quantity: "9" },
+    { id: "15", title: "Pirri 2-pack", price: "2699", quantity: "22" },
+    { id: "16", title: "Melisso", price: "4999", quantity: "15" },
+    { id: "17", title: "Ditti", price: "1999", quantity: "6" },
+  ];
 
-
-   return (
-     <div style={{ 
+  return (
+    <div
+      style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        width: "100%"
-        }}>
-       <div
-         style={{
-           height: 400,
-           width: "50%",
-           paddingBottom: "4rem",
-         }}
-       >
-         <DataGrid
-           rows={rows}
-           columns={columns}
-           pageSize={5}
-           rowsPerPageOptions={[2]}
-           checkboxSelection
-         />
-       </div>
-     </div>
-   );
- }
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          height: 400,
+          width: "50%",
+          paddingBottom: "4rem",
+        }}
+      >
+        <h4>Products in stock</h4>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+        />
+      </div>
+    </div>
+  );
+}
 
-
- 
-  export default ProductList;
+export default ProductList;
