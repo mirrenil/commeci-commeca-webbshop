32e711a547cff0f150eca8050f1f7472f5ee81e6
saralindqvist <sarathereselindqvist@hotmail.com>
2022-03-11 11:53:22 +0100

    prepared layout for styling

--- src/components/CheckoutPage.tsx
+++ src/components/CheckoutPage.tsx
@@ -1,4 +1,64 @@
+import { Box, Button, Typography } from "@mui/material";
+
 function CheckoutPage() {
-  return <div>CHECKOUT PAGE</div>
+  return (
+    <div style={{ display: "flex", flexDirection: "column", background: "red", margin: "3rem"}}>
+      <Typography variant="h5" gutterBottom style={{ background: "yellow", marginTop: "1rem"}}>
+        1. Shopping Cart
+      </Typography>
+      <Box
+      sx={{
+        height: 300,
+        backgroundColor: "yellow",
+      }}
+    />
+
+    <Typography variant="h5" gutterBottom style={{ background: "yellow", marginTop: "1rem"}}>
+        2. Delivery method
+      </Typography>
+      <Box
+      sx={{
+        height: 300,
+        backgroundColor: "yellow",
+      }}
+    />
+
+    <Typography variant="h5" gutterBottom style={{ background: "yellow", marginTop: "1rem"}}>
+        3. Contact details
+      </Typography>
+      <Box
+      sx={{
+        height: 300,
+        backgroundColor: "yellow",
+      }}
+    />
+
+    <Typography variant="h5" gutterBottom style={{ background: "yellow", marginTop: "1rem"}}>
+        4. Payment method
+      </Typography>
+      <Box
+      sx={{
+        height: 300,
+        backgroundColor: "yellow",
+      }}
+    />
+
+    <Button
+            size="large"
+            variant="contained"
+            style={{
+              margin: "2rem",
+              width: "400px",
+              backgroundColor: "#CAC2B9",
+              color: "black",
+              letterSpacing: "3px",
+            }}
+          >
+            Confirm purchase
+    </Button>
+
+    </div>
+  );
 }
+
 export default CheckoutPage;
