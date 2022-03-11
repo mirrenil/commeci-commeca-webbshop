6e329c571d714c6d7d7493a2dd064de560da6cd1
Millie Cheung <cheungmillie9@gmail.com>
2022-03-11 12:19:05 +0100

    added several sum functions for calculating price and quantity of the cart items

--- src/context/CartContextProvider.tsx
+++ src/context/CartContextProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, FC, MouseEvent, useContext, useState } from "react";
+import { createContext, FC, useContext, useState } from "react";
 import { ProductData } from "../ProductData";
 
 interface CartItemData extends ProductData {
@@ -8,26 +8,32 @@ interface ContextValue {
   cart: CartItemData[];
   addToCart: (product: ProductData) => void;
   sumCartQuantity: () => number;
+  sumCartAmount: () => number;
+  sumProductPrice: (product) => number;
+  onAddQuantity: (product) => void;
+  onReduceQuantity: (product) => void;
 }
 
 export const CartContext = createContext<ContextValue>({
   cart: [],
   addToCart: () => {},
   sumCartQuantity: () => 0,
+  sumCartAmount: () => 0,
+  sumProductPrice: () => 0,
+  onAddQuantity: () => {},
+  onReduceQuantity: () => {},
 });
 
 const CartProvider: FC = (props) => {
   const [cart, setCart] = useState<CartItemData[]>([]);
 
   const addToCart = async (product: ProductData) => {
-    console.log("called add to cart");
     // if (cart.some(item => item.id === product.id))
     if (cart.map((item) => item.id).includes(product.id)) {
       const updatedCart = cart.map((item) => {
         if (product.id !== item.id) return item;
         return { ...item, quantity: item.quantity + 1 };
       });
-
       setCart(updatedCart);
     } else {
       const cartItem: CartItemData = { ...product, quantity: 1 };
@@ -37,18 +43,66 @@ const CartProvider: FC = (props) => {
   };
 
   const sumCartQuantity = () => {
-    console.log("called sum");
     let sum = 0;
     for (let i = 0; i < cart.length; i++) {
       sum += cart[i].quantity;
     }
+    sumCartAmount();
     console.log(sum);
     return sum;
     // return cart.reduce((sum, item) => sum + item.quantity, 0);
   };
 
+  const sumCartAmount = () => {
+    let sum = 0;
+    for (let i = 0; i < cart.length; i++) {
+      sum += cart[i].price * cart[i].quantity;
+      console.log(sum);
+    }
+    console.log("total :" + sum);
+    return sum;
+  };
+
+  const sumProductPrice = (product) => {
+    let sum = 0;
+    for (let i = 0; i < cart.length; i++) {
+      sum += product.price * product.quantity;
+    }
+    return sum;
+  };
+
+  const onAddQuantity = (product) => {
+    if (cart.some((item) => item.id === product.id)) {
+      const updatedQuantity = cart.map((item) => {
+        if (product.id !== item.id) return item;
+        return { ...item, quantity: item.quantity + 1 };
+      });
+      setCart(updatedQuantity);
+    }
+  };
+
+  const onReduceQuantity = (product) => {
+    if (cart.some((item) => item.id === product.id)) {
+      const updatedQuantity = cart.map((item) => {
+        if (product.id !== item.id) return item;
+        return { ...item, quantity: item.quantity - 1 };
+      });
+      setCart(updatedQuantity);
+    }
+  };
+
   return (
-    <CartContext.Provider value={{ cart, addToCart, sumCartQuantity }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        sumCartQuantity,
+        sumCartAmount,
+        sumProductPrice,
+        onAddQuantity,
+        onReduceQuantity,
+      }}
+    >
       {props.children}
     </CartContext.Provider>
   );
