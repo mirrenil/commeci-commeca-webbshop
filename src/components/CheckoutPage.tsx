01becf1241581230b51b134e6f7f9e174a9e64f0
saralindqvist <sarathereselindqvist@hotmail.com>
2022-03-11 16:03:28 +0100

    layout for delivery method and form for contactdetails

--- src/components/CheckoutPage.tsx
+++ src/components/CheckoutPage.tsx
@@ -1,45 +1,86 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, FormControl, Input, InputLabel, Typography } from "@mui/material";
+import PostnordLogo from "../assets/images/PostnordLogo.webp"
+import DhlLogo from "../assets/images/DhlLogo.png"
 
 function CheckoutPage() {
   return (
-    <div style={{ display: "flex", flexDirection: "column", background: "red", margin: "3rem"}}>
-      <Typography variant="h5" gutterBottom style={{ background: "yellow", marginTop: "1rem"}}>
+    <div style={{ display: "flex", flexDirection: "column", margin: "3rem"}}>
+      <Typography variant="h5" gutterBottom style={{ marginTop: "1rem"}}>
         1. Shopping Cart
       </Typography>
       <Box
       sx={{
-        height: 300,
-        backgroundColor: "yellow",
+        height: 400,
+        backgroundColor: "#F3F2F0",
       }}
     />
 
-    <Typography variant="h5" gutterBottom style={{ background: "yellow", marginTop: "1rem"}}>
+    <Typography variant="h5" gutterBottom style={{ marginTop: "1rem"}}>
         2. Delivery method
       </Typography>
       <Box
       sx={{
-        height: 300,
-        backgroundColor: "yellow",
+        height: 400,
+        backgroundColor: "#F3F2F0",
       }}
-    />
+      > 
+      <Typography variant="h6" gutterBottom style={{ marginTop: "2rem", marginLeft: "2rem"}}>
+        Choose delivery:
+      </Typography>
+      <Box
+          component="img"
+          display="flex"
+          style={{ height: "25px", margin: "2rem" }}
+          src={PostnordLogo}
+          >
+      </Box>
+        <Box
+          component="img"
+          style={{ height: "25px", margin: "2rem" }}
+          src={DhlLogo}
+        />
+         <Typography variant="body1" gutterBottom style={{marginLeft: "2rem"}}>
+        Pick up on terminal
+      </Typography>
+      </Box>
 
-    <Typography variant="h5" gutterBottom style={{ background: "yellow", marginTop: "1rem"}}>
+    <Typography variant="h5" gutterBottom style={{ marginTop: "1rem"}}>
         3. Contact details
       </Typography>
       <Box
       sx={{
         height: 300,
-        backgroundColor: "yellow",
+        backgroundColor: "#F3F2F0",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center"
       }}
-    />
+      >
+      <FormControl variant="filled" style={{width: "200px", alignItems: "center" }}>
+      <InputLabel htmlFor="component-simple">Name</InputLabel>
+      <Input id="filled-basic" />
+    </FormControl>
+    <FormControl variant="standard">
+      <InputLabel htmlFor="component-simple">Address</InputLabel>
+      <Input id="component-simple" />
+    </FormControl>
+    <FormControl variant="standard">
+      <InputLabel htmlFor="component-simple">Phonenumber</InputLabel>
+      <Input id="component-simple" />
+    </FormControl>
+    <FormControl variant="standard">
+      <InputLabel htmlFor="component-simple">Emailaddress</InputLabel>
+      <Input id="component-simple" />
+    </FormControl>
+    </Box>
 
-    <Typography variant="h5" gutterBottom style={{ background: "yellow", marginTop: "1rem"}}>
+    <Typography variant="h5" gutterBottom style={{ marginTop: "1rem"}}>
         4. Payment method
       </Typography>
       <Box
       sx={{
         height: 300,
-        backgroundColor: "yellow",
+        backgroundColor: "#F3F2F0",
       }}
     />
 
@@ -47,6 +88,7 @@ function CheckoutPage() {
             size="large"
             variant="contained"
             style={{
+              textAlign: "center",
               margin: "2rem",
               width: "400px",
               backgroundColor: "#CAC2B9",
