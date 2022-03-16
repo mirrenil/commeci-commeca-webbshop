cf96633d9edb8ca2864ad8876859185a46a3f184
saralindqvist <sarathereselindqvist@hotmail.com>
2022-03-16 11:02:31 +0100

    added onChange for form in contact detail to to display errors on required fields
    Co-authored-by: Millie Cheung <millie-wy@users.noreply.github.com>

--- src/components/CheckoutPage.tsx
+++ src/components/CheckoutPage.tsx
@@ -14,7 +14,40 @@ import SkrivbordAlba from "../assets/images/SkrivbordAlba.webp";
 import * as yup from "yup";
 import { useFormik } from "formik";
 
+interface FormValues {
+  name: string;
+  email: string;
+  address: string;
+  phonenumber: string;
+}
+
+const InitialValue: FormValues = {
+  name: "Name",
+  address: "Address",
+  email: "Email",
+  phonenumber: "Phonenumber",
+};
+
+const validationSchema = yup.object({
+  name: yup.string().required("Name is required"),
+  address: yup.string().required("Address is required"),
+  email: yup.string().required("Email is required"),
+  phonenumber: yup
+    .string()
+    .min(10, "Phonenumber should be minimum 10 characters"),
+});
+
 function CheckoutPage() {
+  const { values, errors, touched, handleSubmit, handleChange } =
+    useFormik<FormValues>({
+      initialValues: InitialValue,
+      validationSchema: validationSchema,
+      onSubmit: (values) => {
+        alert(JSON.stringify(values, null, 2));
+        console.log("submitted");
+      },
+    });
+
   return (
     <div style={{ display: "flex", flexDirection: "column", margin: "3rem" }}>
       <Typography variant="h5" gutterBottom style={{ marginTop: "1rem" }}>
@@ -47,246 +80,264 @@ function CheckoutPage() {
           <Typography style={{ fontWeight: "bold" }}>2999:-</Typography>
         </Box>
         <Typography
-          style={{ fontWeight: "bold", marginLeft: "2rem", marginTop: "1rem" }}
+          style={{
+            fontWeight: "bold",
+            marginLeft: "2rem",
+            marginTop: "1rem",
+          }}
           variant="h5"
         >
           Totalt:
         </Typography>
       </Box>
-
-      <Typography variant="h5" gutterBottom style={{ marginTop: "2rem" }}>
-        2. Delivery method
-      </Typography>
-      <Box
-        sx={{
-          height: 350,
-          backgroundColor: "#F3F2F0",
-        }}
-      >
-        <Typography
-          variant="h6"
-          gutterBottom
-          style={{ marginTop: "2rem", marginLeft: "2rem" }}
-        >
-          Choose delivery:
+      <form onSubmit={handleSubmit}>
+        <Typography variant="h5" gutterBottom style={{ marginTop: "2rem" }}>
+          2. Delivery method
         </Typography>
-
         <Box
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            marginLeft: "2rem",
+          sx={{
+            height: 350,
+            backgroundColor: "#F3F2F0",
           }}
         >
-          <FormGroup>
-            <FormControlLabel
-              control={<Checkbox defaultChecked />}
-              label={
-                <Box sx={{ display: "flex" }}>
-                  <img src={PostnordLogo} alt="Postnord" height="20px" />
-                  <Typography style={{ marginLeft: "1rem" }}>
-                    495 SEK (3-5 Weekdays)
-                  </Typography>
-                </Box>
-              }
-            />
-            <FormControlLabel
-              control={<Checkbox />}
-              label={
-                <Box sx={{ display: "flex" }}>
-                  <img src={DhlLogo} alt="Postnord" height="20px" />
-                  <Typography style={{ marginLeft: "1rem" }}>
-                    345 SEK (5-7 Weekdays)
-                  </Typography>
-                </Box>
-              }
-            />
-            <FormControlLabel
-              control={<Checkbox />}
-              label={
-                <Box sx={{ display: "flex" }}>
-                  <Typography style={{ fontWeight: "bold" }}>
-                    Pick up on terminal
-                  </Typography>
-                  <Typography style={{ marginLeft: "1rem" }}>
-                    Free (2-3 Weekdays)
-                  </Typography>
-                </Box>
-              }
-            />
-          </FormGroup>
+          <Typography
+            variant="h6"
+            gutterBottom
+            style={{ marginTop: "2rem", marginLeft: "2rem" }}
+          >
+            Choose delivery:
+          </Typography>
+
+          <Box
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              marginLeft: "2rem",
+            }}
+          >
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox defaultChecked />}
+                label={
+                  <Box sx={{ display: "flex" }}>
+                    <img src={PostnordLogo} alt="Postnord" height="20px" />
+                    <Typography style={{ marginLeft: "1rem" }}>
+                      495 SEK (3-5 Weekdays)
+                    </Typography>
+                  </Box>
+                }
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label={
+                  <Box sx={{ display: "flex" }}>
+                    <img src={DhlLogo} alt="Postnord" height="20px" />
+                    <Typography style={{ marginLeft: "1rem" }}>
+                      345 SEK (5-7 Weekdays)
+                    </Typography>
+                  </Box>
+                }
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label={
+                  <Box sx={{ display: "flex" }}>
+                    <Typography style={{ fontWeight: "bold" }}>
+                      Pick up on terminal
+                    </Typography>
+                    <Typography style={{ marginLeft: "1rem" }}>
+                      Free (2-3 Weekdays)
+                    </Typography>
+                  </Box>
+                }
+              />
+            </FormGroup>
+          </Box>
         </Box>
-      </Box>
-      <Typography variant="h5" gutterBottom style={{ marginTop: "2rem" }}>
-        3. Contact details
-      </Typography>
-      <Box
-        sx={{
-          height: 470,
-          backgroundColor: "#F3F2F0",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
+        <Typography variant="h5" gutterBottom style={{ marginTop: "2rem" }}>
+          3. Contact details
+        </Typography>
+        <Box
+          sx={{
+            height: 470,
+            backgroundColor: "#F3F2F0",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "600px",
+            }}
+          >
+            <TextField
+              style={{
+                backgroundColor: "white",
+              }}
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              margin="normal"
+              value={values.name}
+              onChange={handleChange}
+              error={touched.name && Boolean(errors.name)}
+              helperText={errors.name}
+            />
+            <TextField
+              style={{
+                backgroundColor: "white",
+              }}
+              id="address-input"
+              name="address"
+              label="Home Address"
+              type="text"
+              margin="normal"
+              value={values.address}
+              onChange={handleChange}
+              error={touched.address && Boolean(errors.address)}
+              helperText={errors.address}
+            />
+            <TextField
+              style={{
+                backgroundColor: "white",
+              }}
+              id="email-input"
+              name="email"
+              label="Email"
+              type="text"
+              margin="normal"
+              value={values.email}
+              onChange={handleChange}
+              error={touched.address && Boolean(errors.email)}
+              helperText={errors.email}
+            />
+            <TextField
+              style={{
+                backgroundColor: "white",
+              }}
+              id="phonennumber-input"
+              name="phonenumber"
+              label="Phonenumber"
+              type="text"
+              margin="normal"
+              value={values.phonenumber}
+              onChange={handleChange}
+              error={touched.phonenumber && Boolean(errors.phonenumber)}
+              helperText={errors.phonenumber}
+            />
+          </Box>
+        </Box>
+
+        <Typography variant="h5" gutterBottom style={{ marginTop: "2rem" }}>
+          4. Payment method
+        </Typography>
+        <Box
+          sx={{
+            height: 300,
+            backgroundColor: "#F3F2F0",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Typography
+            variant="h6"
+            gutterBottom
+            style={{
+              margin: "2rem",
+            }}
+          >
+            Choose Payment Method:
+          </Typography>
+          <Box
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              marginLeft: "2rem",
+              marginBottom: "1rem",
+            }}
+          >
+            <Checkbox />
+            <Typography style={{ marginLeft: "1rem", fontWeight: "bold" }}>
+              Kort
+            </Typography>
+          </Box>
+
+          <Box
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              marginLeft: "2rem",
+              marginBottom: "1rem",
+            }}
+          >
+            <Checkbox />
+            <Box
+              component="img"
+              display="flex"
+              style={{
+                height: "20px",
+                justifyContent: "center",
+                marginLeft: "1rem",
+              }}
+              src={SwishLogo}
+            />
+          </Box>
+
+          <Box
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              marginLeft: "2rem",
+            }}
+          >
+            <Checkbox />
+            <Typography style={{ marginLeft: "1rem", fontWeight: "bold" }}>
+              Faktura
+            </Typography>
+          </Box>
+        </Box>
+
         <Box
           sx={{
             display: "flex",
+            justifyContent: "center",
             flexDirection: "column",
-            width: "600px",
-          }}
-        >
-          <TextField
-            style={{
-              backgroundColor: "white",
-            }}
-            id="name-input"
-            name="name"
-            label="Name"
-            type="text"
-            color="primary"
-            margin="normal"
-          />
-          <TextField
-            style={{
-              backgroundColor: "white",
-            }}
-            id="address-input"
-            name="address"
-            label="Home Address"
-            type="text"
-            color="primary"
-            margin="normal"
-          />
-          <TextField
-            style={{
-              backgroundColor: "white",
-            }}
-            id="email-input"
-            name="email"
-            label="Emailaddress"
-            type="text"
-            color="primary"
-            margin="normal"
-          />
-          <TextField
-            style={{
-              backgroundColor: "white",
-            }}
-            id="phonennumber-input"
-            name="phonenumber"
-            label="Phonenumber"
-            type="text"
-            color="primary"
-            margin="normal"
-          />
-        </Box>
-      </Box>
-
-      <Typography variant="h5" gutterBottom style={{ marginTop: "2rem" }}>
-        4. Payment method
-      </Typography>
-      <Box
-        sx={{
-          height: 300,
-          backgroundColor: "#F3F2F0",
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-        }}
-      >
-        <Typography
-          variant="h6"
-          gutterBottom
-          style={{
-            margin: "2rem",
-          }}
-        >
-          Choose Payment Method:
-        </Typography>
-        <Box
-          style={{
-            display: "flex",
-            flexDirection: "row",
             alignItems: "center",
-            marginLeft: "2rem",
-            marginBottom: "1rem",
           }}
         >
-          <Checkbox />
-          <Typography style={{ marginLeft: "1rem", fontWeight: "bold" }}>
-            Kort
+          <Typography
+            variant="h5"
+            style={{ marginTop: "2rem", fontWeight: "bold" }}
+          >
+            Total:
           </Typography>
-        </Box>
-
-        <Box
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            marginLeft: "2rem",
-            marginBottom: "1rem",
-          }}
-        >
-          <Checkbox />
-          <Box
-            component="img"
-            display="flex"
+          <Button
+            size="large"
+            variant="contained"
             style={{
-              height: "20px",
-              justifyContent: "center",
-              marginLeft: "1rem",
+              textAlign: "center",
+              margin: "2rem",
+              width: "400px",
+              backgroundColor: "#CAC2B9",
+              color: "white",
+              letterSpacing: "3px",
             }}
-            src={SwishLogo}
-          />
+            type="submit"
+          >
+            Confirm purchase
+          </Button>
         </Box>
-
-        <Box
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            marginLeft: "2rem",
-          }}
-        >
-          <Checkbox />
-          <Typography style={{ marginLeft: "1rem", fontWeight: "bold" }}>
-            Faktura
-          </Typography>
-        </Box>
-      </Box>
-
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography
-          variant="h5"
-          style={{ marginTop: "2rem", fontWeight: "bold" }}
-        >
-          Total:
-        </Typography>
-        <Button
-          size="large"
-          variant="contained"
-          style={{
-            textAlign: "center",
-            margin: "2rem",
-            width: "400px",
-            backgroundColor: "#CAC2B9",
-            color: "white",
-            letterSpacing: "3px",
-          }}
-        >
-          Confirm purchase
-        </Button>
-      </Box>
+      </form>
     </div>
   );
 }
