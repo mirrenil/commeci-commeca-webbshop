dea55721d4b7956a7d59baf45ec11839c854d8c5
Millie Cheung <cheungmillie9@gmail.com>
2022-03-17 13:45:43 +0100

    added styling for small andn medium screen

--- src/components/DetailPage.tsx
+++ src/components/DetailPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import detailInfo from "../assets/images/detailinfo.png";
 import { productData } from "../ProductData";
@@ -10,15 +10,18 @@ function DetailPage() {
   if (!product) return null;
 
   return (
-    <div
+    <Container
       style={{
         display: "flex",
         justifyContent: "center",
         marginTop: "4rem",
         marginBottom: "4rem",
       }}
+      sx={{
+        flexDirection: { xs: "column", sm: "column", md: "row", lg: "row" },
+      }}
     >
-      <Box
+      <Container
         component="img"
         height="300"
         style={{
@@ -30,70 +33,56 @@ function DetailPage() {
         src={product.image}
         title={product.title}
         id={product.id}
-      ></Box>
-      <div
+      ></Container>
+      <Container
         style={{
-          marginLeft: "7rem",
           display: "flex",
           flexDirection: "column",
+          width: "fit-content",
+        }}
+        sx={{
+          marginLeft: { md: 0, lg: "2rem" },
+          marginTop: { sm: 0, md: "4rem", lg: "4rem" },
         }}
       >
-        <Box style={{ width: "500px" }}>
-          <Typography
-            variant="h3"
-            gutterBottom
-            style={{ fontSize: "2rem", marginLeft: "2rem", marginTop: "4rem" }}
-          >
-            {product.title}
-          </Typography>
-          <Typography
-            variant="h6"
-            gutterBottom
-            style={{
-              fontSize: "13px",
-              marginLeft: "2rem",
-              marginTop: "1rem",
-              marginBottom: "2rem",
-              maxWidth: "400px",
-              color: "#545454",
-            }}
-          >
-            {product.description}
-          </Typography>
-          <Typography
-            variant="h4"
-            gutterBottom
-            style={{
-              marginLeft: "2.3rem",
-              fontSize: "1.4rem",
-            }}
-          >
-            {product.price} SEK
-          </Typography>
-          <AddToCartButton
-            product={product}
-            size="large"
-            style={{
-              margin: "2rem",
-              width: "400px",
-              backgroundColor: "#CAC2B9",
-              letterSpacing: "3px",
-            }}
-          />
-        </Box>
-        <Box
-          component="img"
-          height="300"
+        <Typography variant="h3" gutterBottom style={{ fontSize: "2rem" }}>
+          {product.title}
+        </Typography>
+        <Typography
+          variant="h6"
+          gutterBottom
           style={{
-            height: "130px",
-            width: "350px",
-            maxWidth: "500px",
-            marginLeft: "2.2rem",
+            fontSize: "13px",
+            marginTop: "1rem",
+            marginBottom: "2rem",
+            maxWidth: "400px",
+            color: "#545454",
           }}
-          src={detailInfo}
-        ></Box>
-      </div>
-    </div>
+        >
+          {product.description}
+        </Typography>
+        <Typography
+          variant="h4"
+          gutterBottom
+          style={{
+            fontSize: "1.4rem",
+          }}
+        >
+          {product.price} SEK
+        </Typography>
+        <AddToCartButton
+          product={product}
+          size="large"
+          style={{
+            margin: "2rem",
+            width: "360px",
+            backgroundColor: "#CAC2B9",
+            letterSpacing: "3px",
+          }}
+        />
+        <img src={detailInfo} alt="product details" width="350px" />
+      </Container>
+    </Container>
   );
 }
 
