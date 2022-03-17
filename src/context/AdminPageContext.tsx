24d6e0d886f3fb82b6e0ea90fb2b847e734826ae
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2022-03-17 11:47:02 +0100

    Admin delete function works in admin page and product page
    Co-authored-by: Miranda Nilhag <mirrenil@users.noreply.github.com>
    Co-authored-by: Sara Lindqvist <saralindqvist@users.noreply.github.com>

--- src/context/AdminPageContext.tsx
+++ src/context/AdminPageContext.tsx
@@ -1,48 +1,57 @@
 import { propsToClassKey } from "@mui/styles";
 import { render } from "@testing-library/react";
 import { createContext, FC, useContext, useState } from "react";
+import { isTemplateMiddle } from "typescript";
 import { useLocalStorageState } from "../components/hooks/useLocalStorageState";
 import { ProductData, productData } from "../ProductData";
 
-// interface AdminData extends ProductData {
-//     quantity: number;
-// }
+interface AdminData extends ProductData {
+}
 
 interface ContextValue {
-    products: ProductData[];
-    addProduct:  (product) => void;
-    removeProduct: (product) => void;
+  products: ProductData[];
+  addProduct: (product: ProductData) => void;
+  removeProduct: (product: ProductData) => void;
 }
 
 export const ProductContext = createContext<ContextValue>({
     products: [],
-    addProduct: (product: ProductData) => [],
-    removeProduct: (product: ProductData) => [],
+    addProduct: (product) => [],
+    removeProduct: (product) => [],
 });
 
 const ProductProvider = (props) => {
+    
+    // This line controlls how many rows should be shown, in this case 4
     const [products, setProducts] = useLocalStorageState(productData, "adminLS");
-    console.log('hej');
     
-    
-    //const [products, setproducts] = useState<Product[]>(productData);
-   
-   const addProduct = (product: ProductData) => {
+ 
+
+    const addProduct = (product: ProductData) => {
         setProducts([...products, product]);
     }
-   
+
+        const removeProduct = (productToBeRemoved: ProductData) => {
+
+          const updatedProductList = products.filter((product) => productToBeRemoved.id !== product.id)
+
+
+            updateProductsToLS()
+            
+            setProducts(updatedProductList);
+       
+           
+        };
+        
+
     
-    const removeProduct = (product: ProductData) => {
-        //setProducts(products.filter(({id}) => id !== product.id));
-        alert('YOU CLICKED DELETE')
-    };
 
     return (
         <ProductContext.Provider
             value={{
             products,
             addProduct,
-            removeProduct,
+          removeProduct,
             }}
         >
         {props.children}
@@ -53,4 +62,11 @@ const ProductProvider = (props) => {
 
 export default ProductProvider;
 export const useAdmin = () => useContext(ProductContext);
+
+  
+
+    function updateProductsToLS() {
+        const productsAsString = JSON.stringify(productData);
+        localStorage.setItem("productsHELLO", productsAsString)
+    }
 
