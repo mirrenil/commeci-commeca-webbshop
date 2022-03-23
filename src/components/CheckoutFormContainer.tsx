f2c56b91b1f9ecf7c60444db7df300c4c5791f8c
Millie Cheung <cheungmillie9@gmail.com>
2022-03-23 12:26:11 +0100

    tried to add conditional validation to payment methods (not done yet)

--- src/components/CheckoutFormContainer.tsx
+++ src/components/CheckoutFormContainer.tsx
@@ -1,4 +1,5 @@
 import { Box, Button } from "@mui/material";
+import valid from "card-validator";
 import { Form, Formik } from "formik";
 import { useNavigate } from "react-router-dom";
 import * as yup from "yup";
@@ -8,7 +9,6 @@ import DeliveryDetails from "./DeliveryDetails";
 import DeliveryOptions from "./DeliveryOptions";
 import PaymentMethod from "./PaymentMethod";
 import PriceOverview from "./PriceOverview";
-import valid from "card-validator";
 
 export interface FormValues {
   name: string;
@@ -33,50 +33,84 @@ const InitialValue: FormValues = {
   invoice: "",
 };
 
-const phoneRegExp = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
-const personalIdentityRegExp =
-  /^(19|20)?(\d{6}([-+]|\s)\d{4}|(?!19|20)\d{10})$/;
-
-const ContactValidationSchema = yup.object({
-  name: yup.string().min(2).required("Required"),
-  address: yup.string().min(5).required("Required"),
-  email: yup.string().email("Invalid email").required("Required"),
-  phoneNumber: yup
-    .string()
-    .required("Required")
-    .matches(phoneRegExp, "Invalid phone number"),
-  cardNumber: yup
-    .string()
-    .test(
-      "test-number",
-      "Invalid card number",
-      (value) => valid.number(value).isValid
-    ),
-  cardExpiry: yup
-    .string()
-    .test(
-      "test-number",
-      "Invalid",
-      (value) => valid.expirationDate(value).isValid
-    ),
-  cardCVC: yup
-    .string()
-    .test("test-number", "Invalid", (value) => valid.cvv(value).isValid),
-  swish: yup.string().matches(phoneRegExp, "Invalid phone number"),
-  invoice: yup
-    .string()
-    .matches(personalIdentityRegExp, "Invalid personal identity number"),
-});
-
 function CheckoutFormContainer() {
   const navigate = useNavigate();
-  const { emptyCart } = useCart();
+  const { emptyCart, paymentMethod } = useCart();
   const { createOrder } = useOrder();
+
+  const phoneRegExp = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+  const personalIdentityRegExp =
+    /^(19|20)?(\d{6}([-+]|\s)\d{4}|(?!19|20)\d{10})$/;
+
+  console.log(
+    "Credit Card: " + Boolean(paymentMethod === "creditcard").toString()
+  );
+  console.log("Swish: " + Boolean(paymentMethod === "swish").toString());
+  console.log("Invoice: " + Boolean(paymentMethod === "invoice").toString());
+
+  const ValidationSchema = yup.object({
+    name: yup.string().min(2).required("Required"),
+    address: yup.string().min(5).required("Required"),
+    email: yup.string().email("Invalid email").required("Required"),
+    phoneNumber: yup
+      .string()
+      .required("Required")
+      .matches(phoneRegExp, "Invalid phone number"),
+
+    cardNumber: yup.string().when("paymentMethod", {
+      is: (paymentMethod) => paymentMethod === "creditcard",
+      then: yup
+        .string()
+        .test(
+          "test-number",
+          "Invalid card number",
+          (value) => valid.number(value).isValid
+        ),
+      otherwise: yup.string(),
+    }),
+
+    cardExpiry: yup.string().when("paymentMethod", {
+      is: (paymentMethod) => paymentMethod === "creditcard",
+      then: yup
+        .string()
+        .test(
+          "test-number",
+          "Invalid",
+          (value) => valid.expirationDate(value).isValid
+        ),
+      otherwise: yup.string(),
+    }),
+
+    cardCVC: yup.string().when("paymentMethod", {
+      is: (paymentMethod) => paymentMethod === "creditcard",
+      then: yup
+        .string()
+        .test("test-number", "Invalid", (value) => valid.cvv(value).isValid),
+      otherwise: yup.string(),
+    }),
+
+    swish: yup.string().when(Boolean(paymentMethod === "swish").toString(), {
+      is: true,
+      then: yup
+        .string()
+        .matches(phoneRegExp, "Invalid phone number")
+        .required("this must be filled"),
+      otherwise: yup.string().required("this is required"),
+    }),
+
+    invoice: yup.string().when("paymentMethod", {
+      is: (paymentMethod) => paymentMethod === "invoice",
+      then: yup
+        .string()
+        .matches(personalIdentityRegExp, "Invalid personal identity number"),
+      otherwise: yup.string(),
+    }),
+  });
 
   return (
     <Formik
       initialValues={InitialValue}
-      validationSchema={ContactValidationSchema}
+      validationSchema={ValidationSchema}
       onSubmit={(values: FormValues) => {
         let promise = new Promise((resolve) => {
           setTimeout(() => {
