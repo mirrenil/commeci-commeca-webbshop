9ef38332743d3797ba5d35c2c923f2bde04e0217
Millie Cheung <cheungmillie9@gmail.com>
2022-03-24 11:46:08 +0100

    removed footer css and adjusted styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,71 +1,79 @@
-import { Box, Container, Grid, Link } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
-import FacebookIcon from "@mui/icons-material/Facebook";
-export function Footer() {
+import { Box, Container, Grid, Link, Typography } from "@mui/material";
+
+function Footer() {
   return (
     <footer>
       <Box
         px={{ xs: 3, sm: 10 }}
-        py={{ xs: 5, sm: 10 }}
+        py={{ xs: 5, sm: 8 }}
         bgcolor="#6C665F"
         color="white"
       >
         <Container maxWidth="lg">
-          <Grid container spacing={5}>
+          <Grid container spacing={3}>
             <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>Customer Care</Box>
-              <Box>
-                <Link href="/" color="inherit">
+              <Box borderBottom={1}>
+                <Typography variant="subtitle1">Customer Care</Typography>
+              </Box>
+
+              <Link underline="none" href="/" color="inherit">
+                <Typography variant="body2" mt="1rem">
                   Contact us
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
+                </Typography>
+              </Link>
+              <Link underline="none" href="/" color="inherit">
+                <Typography variant="body2" mt=".2rem">
                   Support
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
+                </Typography>
+              </Link>
+              <Link underline="none" href="/" color="inherit">
+                <Typography variant="body2" mt=".2rem">
                   FAQ
-                </Link>
-              </Box>
+                </Typography>
+              </Link>
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>Collection</Box>
-              <Box>
-                <Link href="/" color="inherit">
+              <Box borderBottom={1}>
+                <Typography variant="subtitle1">Collection</Typography>
+              </Box>
+              <Link underline="none" href="/" color="inherit">
+                <Typography variant="body2" mt="1rem">
                   The Lookbook
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
+                </Typography>
+              </Link>
+              <Link underline="none" href="/" color="inherit">
+                <Typography variant="body2" mt=".2rem">
                   2022 Spring Collection
-                </Link>
-              </Box>
+                </Typography>
+              </Link>
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>About us</Box>
-              <Box>
-                <Link href="/" color="inherit">
+              <Box borderBottom={1}>
+                <Typography variant="subtitle1">About Us</Typography>
+              </Box>
+              <Link underline="none" href="/" color="inherit">
+                <Typography variant="body2" mt="1rem">
                   Design philiosophy
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
+                </Typography>
+              </Link>
+              <Link underline="none" href="/" color="inherit">
+                <Typography variant="body2" mt=".2rem">
                   Find a store near you
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
+                </Typography>
+              </Link>
+              <Link underline="none" href="/" color="inherit">
+                <Typography variant="body2" mt=".2rem">
                   Work with us
-                </Link>
-              </Box>
+                </Typography>
+              </Link>
             </Grid>
           </Grid>
           <Box
             textAlign="center"
-            pt={{ xs: 5, sm: 10 }}
+            pt={{ xs: 5, sm: 8 }}
             pb={{ xs: 5, sm: 0 }}
             display="flex"
             justifyContent="space-evenly"
@@ -74,7 +82,7 @@ export function Footer() {
             <InstagramIcon />
             <FacebookIcon />
           </Box>
-          <Box textAlign="center" pt={{ xs: 5, sm: 10 }} pb={{ xs: 5, sm: 0 }}>
+          <Box textAlign="center" pt={{ xs: 2, sm: 4 }} pb={{ xs: 2, sm: 0 }}>
             comme ci comme ça &reg; {new Date().getFullYear()}
           </Box>
         </Container>
