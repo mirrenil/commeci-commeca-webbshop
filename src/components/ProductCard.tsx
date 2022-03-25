476025f39635ccc29626a9373b74b210255f93f3
Millie Cheung <cheungmillie9@gmail.com>
2022-03-25 10:05:33 +0100

    replace props with admin context and got back the styling

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Card,
   CardActionArea,
   CardActions,
@@ -9,63 +10,75 @@ import {
 } from "@mui/material";
 import { CSSProperties } from "react";
 import { Link } from "react-router-dom";
-import { ProductData } from "../ProductData";
+import { useAdmin } from "../context/AdminPageContext";
 import AddToCartButton from "./shared/AddToCartButton";
 
-interface Props {
-  product: ProductData;
-}
+function ProductCard() {
+  const { products } = useAdmin();
 
-function ProductCard(props: Props) {
   return (
-    <Card sx={cardStyle}>
-      <Link to={`/detail/${props.product.id}`} style={linkStyle}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="250"
-            style={imgStyle}
-            src={props.product.image}
-            title={props.product.title}
-            id={props.product.id}
-          ></CardMedia>
-        </CardActionArea>
-        <Typography
-          style={{ marginTop: "1rem" }}
-          gutterBottom
-          variant="h5"
-          component="h2"
-        >
-          {props.product.title}
-        </Typography>
-      </Link>
-      <div
-        style={{
-          display: "flex",
-          gap: "2rem",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="body2" color="text.secondary">
-          {props.product.price} SEK
-        </Typography>
-        <CardActions>
-          <AddToCartButton product={props.product} size="small" />
-        </CardActions>
-      </div>
-    </Card>
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "3rem 10rem",
+        justifyContent: "center",
+        paddingTop: "2rem",
+        paddingBottom: "6rem",
+      }}
+    >
+      {products.map((product) => (
+        <Card sx={cardStyle}>
+          <Link to={`/detail/${product.id}`} style={linkStyle}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="250"
+                style={imgStyle}
+                src={product.image}
+                title={product.title}
+                id={product.id}
+              ></CardMedia>
+            </CardActionArea>
+            <Typography
+              style={{ marginTop: "1rem" }}
+              gutterBottom
+              variant="h5"
+              component="h2"
+            >
+              {product.title}
+            </Typography>
+          </Link>
+          <Box
+            style={{
+              display: "flex",
+              gap: "2rem",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="body2" color="text.secondary">
+              {product.price} SEK
+            </Typography>
+            <CardActions>
+              <AddToCartButton product={product} size="small" />
+            </CardActions>
+          </Box>
+        </Card>
+      ))}
+    </Box>
   );
 }
 
 const cardStyle: SxProps<Theme> = {
   width: 250,
   padding: "1rem",
-  maxWidth: 345,
   boxShadow: "none",
 };
+
 const imgStyle: CSSProperties = {
   height: 350,
 };
+
 const linkStyle: CSSProperties = {
   textDecoration: "none",
   color: "#333",
--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,46 +1,25 @@
-import { Container, Typography } from "@mui/material";
-import React from "react";
-import { CSSProperties } from "react";
-import { ProductContext } from "../context/AdminPageContext";
-import "../index.css";
+import { Box, Container, Typography } from "@mui/material";
 import ProductCard from "./ProductCard";
 
- 
 function ProductPage() {
-  const products = React.useContext(ProductContext).products;
-  
   return (
-    <div>
-      <Container
-        sx={{
-          padding: "1rem",
-        }}
-      >
+    <Box>
+      <Container>
         <Typography
-          sx={{ textTransform: "uppercase", fontFamily: "Prata", mt: "1rem" }}
+          sx={{
+            padding: "1rem 0 ",
+            textTransform: "uppercase",
+            fontFamily: "Prata",
+            mt: "1rem",
+          }}
           variant="h5"
         >
           Furniture
         </Typography>
       </Container>
-      <div style={rootStyle}>
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-     
-    </div>
+      <ProductCard />
+    </Box>
   );
 }
-
-const rootStyle: CSSProperties = {
-  display: "flex",
-  flexWrap: "wrap",
-  columnGap: "10rem",
-  rowGap: "3rem",
-  justifyContent: "center",
-  paddingTop: "2rem",
-  paddingBottom: "6rem",
-};
 
 export default ProductPage;
